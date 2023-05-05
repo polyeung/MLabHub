6377ce9f231988c84839da6ffbbd198d9db520c3
liyang <liyangg@umich.edu>
2023-05-05 00:08:39 -0400

    add confirm password and check password string

--- src/pages/signupPage/index.tsx
+++ src/pages/signupPage/index.tsx
@@ -9,6 +9,8 @@ function SignupPage() {
 	const [waiting, setWaiting] = useState(false);
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [secPass, setSecPass] = useState('');
+	//const [passSame, setPassSame]
 	{ /*Username only alow alphabet, number and dash */}
 	function handleChangeUsername(e: any) { 
 		const ALPHA_NUMERIC_DASH_REGEX = /^[a-zA-Z0-9-]+$/;
@@ -17,8 +19,31 @@ function SignupPage() {
 		}
 		setUsername(e.target.value)
 	};
+	function handleChangePass(e: any) { 
+		const ALPHA_NUMERIC_SPECIAL_REGEX = /^[a-zA-Z0-9!@#$%^&*()]+$/;
+		if (e.target.value !== "" && !ALPHA_NUMERIC_SPECIAL_REGEX.test(e.target.value)) {
+			return;
+		}
+		setPassword(e.target.value)
+	}
+	function handleChangeConfPass(e: any) { 
+		const ALPHA_NUMERIC_SPECIAL_REGEX = /^[a-zA-Z0-9!@#$%^&*()]+$/;
+		if (e.target.value !== "" && !ALPHA_NUMERIC_SPECIAL_REGEX.test(e.target.value)) {
+			return;
+		}
+		setSecPass(e.target.value)
+	}
 	const handleSubmit = (e: any) => {
 		e.preventDefault();
+		// error checking
+		if (password !== secPass) { 
+			notifs.addNotif(
+				{
+					severity: 'error',
+					message: 'Confirm password not the same!'
+				});
+			return;
+		}
 		setWaiting(true);
 		fetch('http://localhost:8000/api/account/create/', {
 			method: 'POST',
@@ -81,7 +106,20 @@ function SignupPage() {
 								type="password"
 								id="password"
 								value={password}
-								onChange={e => setPassword(e.target.value)}
+								onChange={e => handleChangePass(e)}
+							/>
+
+							<TextField
+								variant="outlined"
+								margin="normal"
+								required
+								fullWidth
+								name="password"
+								label="Confirm Password"
+								type="password"
+								id="confirm-password"
+								value={secPass}
+								onChange={e => handleChangeConfPass(e) }
 							/>
 							
 							<Button
