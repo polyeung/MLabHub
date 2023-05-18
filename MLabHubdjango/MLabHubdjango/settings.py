4ede54e5d061c0f4e0169f021d86bed759ee4ad8
liyang <liyangg@umich.edu>
2023-05-17 22:49:28 -0400

    make django to serve react content

--- MLabHubdjango/MLabHubdjango/settings.py
+++ MLabHubdjango/MLabHubdjango/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'MLabHubdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR, '../dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, '../dist/assets')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
--- MLabHubdjango/MLabHubdjango/urls.py
+++ MLabHubdjango/MLabHubdjango/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-
+from . import views
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
+    path('', views.index, name='indexx')
 ]
 
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name = 'index.html'))]
+#urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name = 'index.html'))]
