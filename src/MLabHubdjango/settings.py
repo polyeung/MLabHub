97b30e493f20413afe13214c62f8052d7760d559
liyang <liyangg@umich.edu>
2023-06-05 19:05:23 -0400

    complete dockerization

--- src/MLabHubdjango/settings.py
+++ src/MLabHubdjango/settings.py
@@ -157,16 +157,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'bundles')
-]
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'bundles'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Django webpack loader
 WEBPACK_LOADER = {
     'DEFAULT': {
-        'CACHE': not DEBUG,
+        'CACHE': True, #TODO: change this to not DEBUG and set debug variable
         'BUNDLE_DIR_NAME': '/',  # must end with slash
         'STATS_FILE': os.path.join(BASE_DIR, 'frontend', 'webpack-stats.json'),
         'POLL_INTERVAL': 0.1,
