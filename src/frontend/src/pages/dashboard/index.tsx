91fd94dfce6c95317aec16a79bfe265da88a2673
Yang (Leon) Li <ly842605942@gmail.com>
2023-06-13 22:15:44 -0400

    add posted lab page

--- src/frontend/src/pages/dashboard/index.tsx
+++ src/frontend/src/pages/dashboard/index.tsx
@@ -12,6 +12,7 @@ import ApprovalIcon from '@mui/icons-material/Approval';
 // panel pages start
 import LabPanel from '@/pages/labPanel';
 import JobPanel from '@/pages/jobPanel';
+import PostedLabPanel from '@/pages/postedLabPanel';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -151,7 +152,7 @@ function dashboard(props: { userData: UserData |  null | undefined; }) {
         <JobPanel/>
       </TabPanel>
       <TabPanel value={value} index={2}>
-        Posted Labs
+       <PostedLabPanel />
         </TabPanel>
         <TabPanel value={value} index={3}>
         Posted Jobs
--- src/frontend/src/pages/postedLabPanel/index.tsx
+++ src/frontend/src/pages/postedLabPanel/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import { CardHeader } from '@mui/material';
+import { CardHeader, CardActions} from '@mui/material';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router-dom';
@@ -12,15 +12,28 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import IconButton from '@mui/material/IconButton';
 import { ScreenContext } from '@/screenContext';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import PendingIcon from '@mui/icons-material/Pending';
+import { styled } from '@mui/material/styles';
+import { ButtonProps } from '@mui/material/Button';
+import { yellow } from '@mui/material/colors';
 
 interface labCardProps { 
-  name: string,
+    name: string,
     dep: string,
+    isPending: boolean
 };
 
+const WarningButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  color: 'text.primary',
+  backgroundColor: yellow[700],
+  '&:hover': {
+    backgroundColor: yellow[900],
+    },
+}));
 
 
-function LabCard({ name, dep}: labCardProps) {
+function LabCard({ name, dep, isPending}: labCardProps) {
   const navigate = useNavigate();
   const { isSmallScreen, isMiddleScreen } = React.useContext(ScreenContext);
     function handleClick() { 
@@ -44,11 +57,12 @@ function LabCard({ name, dep}: labCardProps) {
 
   return (
       <Card sx={{ minWidth: 275, marginBottom: '10px' }} >
-      <CardHeader
+      <CardHeader onClick={handleClick}
         action={
           <ButtonGroup size="small" aria-label="small button group" orientation={isSmallScreen ? "vertical" : "horizontal"}>
             {buttons}
           </ButtonGroup>
+        
         }
         title={
           <Typography component={'span'} variant="h6" >
@@ -57,7 +71,12 @@ function LabCard({ name, dep}: labCardProps) {
         }
         // TODO: add department subtittle
         subheader={dep}
-      />
+          />
+          <CardActions>
+              {isPending ?<WarningButton startIcon={<PendingIcon />} variant="contained" size="small">Pending</WarningButton>
+                  :<Button startIcon={<CheckBoxIcon />} variant="contained" size="small" color="success">Posted</Button>
+              }
+          </CardActions>
     </Card>
   );
 }
@@ -66,24 +85,33 @@ function LabCard({ name, dep}: labCardProps) {
 const labData = [
     {
       name: 'AI Lab',
-      dep: 'EECS'
+      dep: 'EECS',
+      isPending: true
     },
     {
       name: 'AI Lab2',
-      dep: 'EECS'
+        dep: 'EECS',
+        isPending: false
     },
     {
       name: 'AI Lab3',
-      dep: 'EECS'
+        dep: 'EECS',
+        isPending: true
     },
     {
       name: 'AI Lab4',
-      dep: 'EECS'
+        dep: 'EECS',
+        isPending: false
     }
   ];
-export default function labPanel() { 
+export default function postedLabPanel() { 
     return (
-        <>{labData.map((item, index) => (<LabCard key={ "labcard-"+ String(index)} name={item.name} dep={item.dep}/>))
-    }</>
+        <>{labData.map((item, index) => (
+            <LabCard
+                key={"labcard-" + String(index)}
+                name={item.name}
+                dep={item.dep}
+                isPending={item.isPending} />))
+            }</>
     );
 };
