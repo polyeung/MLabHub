cad7d251dc4dd151bcad91c87bb8f278ce9638b8
Elijah Qi <1511791320@qq.com>
2023-11-14 20:02:28 -0500

    Add test to test_is_login

--- src/account/tests.py
+++ src/account/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .views import CheckAuthenticatedView
 
-# Create your tests here.
+class CheckAuthenticatedViewTest(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+    def test_authenticated_user(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get('/path/to/your/view')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('username', response.data)
+        self.assertEqual(response.data['username'], self.user.username)
+
+    def test_unauthenticated_user(self):
+        response = self.client.get('/path/to/your/view')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'isAuthenticated': 'error'})
+
